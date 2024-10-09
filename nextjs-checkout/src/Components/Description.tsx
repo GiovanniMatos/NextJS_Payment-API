634053b0104d5a8356eaa38a8f79d1b070601a3c
GiovanniMatos <giovannyafonso23@gmail.com>
2024-10-09 11:42:40 -0400

    interface update

---
+++ nextjs-checkout/src/Components/Description.tsx
@@ -0,0 +1,33 @@
+
+
+export default function Description(){
+    return(
+        <>
+            <div className="container flex justify-between mb-4">
+                <div className="lg:w-3/4 text-lg">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Sed et libero id justo ullamcorper vulputate vel et mauris. 
+                        Nulla facilisi. Donec quis turpis et lectus ullamcorper rutrum.
+                    </p>
+                </div>
+                <div className="lg:w-1/4">
+                    <img src="iphone-img-teste.jpg" alt="Image"/>
+                </div>
+            </div>
+
+            <div className="container flex justify-between">
+                <div className="lg:w-1/4">
+                    <img src="iphone-img-teste.jpg" alt="Image"/>
+                </div>
+                <div className="lg:w-3/4 text-lg">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                        Sed et libero id justo ullamcorper vulputate vel et mauris. 
+                        Nulla facilisi. Donec quis turpis et lectus ullamcorper rutrum.
+                    </p>
+                </div>
+            </div>
+        </>
+    );
+}
--- nextjs-checkout/src/app/page.tsx
+++ nextjs-checkout/src/app/page.tsx
@@ -1,34 +1,44 @@
 // import Image from "next/image";
+import Description from "../Components/Description";
+import Footer from "../Components/Footer";
 
 export default function Home() {
   return (
-    <div className="container mx-auto p-4 pt-6">
-      <h2 className="text-3xl font-bold mb-4">Checkout</h2>
-      <div className="flex flex-wrap justify-center">
-        <div className="w-full lg:w-1/2 xl:w-1/3 p-6 text-lg">
-          <img src="iphone-img-teste.jpg" alt="Image" className="w-full h-full object-cover object-center"/>
+    <>
+      <div className="container mx-auto p-4 pt-6">
+        <h2 className="text-3xl font-bold mb-4">Checkout</h2>
+        <div className="flex flex-wrap justify-center">
+          <div className="w-full lg:w-1/2 xl:w-1/3 p-6 text-lg">
+            <img src="iphone-img-teste.jpg" alt="Image" className="w-full h-full object-cover object-center"/>
+          </div>
+          <div className="w-full lg:w-1/2 xl:w-2/3 p-6 text-lg">
+            <h2 className="text-2xl font-bold mb-2">Product name</h2>
+            <p className="mb-4">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+              Sed et libero id justo ullamcorper vulputate vel et mauris. 
+              Nulla facilisi. Donec quis turpis et lectus ullamcorper rutrum.
+            </p>
+            <form>
+              <div className="mb-4">
+                <input type="text" id="name" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Name"/>
+              </div>
+              <div className="mb-4">
+                <input type="email" id="email" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Email"/>
+              </div>
+              <div className="mb-4">
+                <input type="text" id="address" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Address"/>
+              </div>
+              <button type="submit" className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Pay Now</button>
+            </form>
+          </div>
         </div>
-        <div className="w-full lg:w-1/2 xl:w-2/3 p-6 text-lg">
-          <h2 className="text-2xl font-bold mb-2">Product name</h2>
-          <p className="mb-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Sed et libero id justo ullamcorper vulputate vel et mauris. 
-            Nulla facilisi. Donec quis turpis et lectus ullamcorper rutrum.
-          </p>
-          <form>
-            <div className="mb-4">
-              <input type="text" id="name" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Name"/>
-            </div>
-            <div className="mb-4">
-              <input type="email" id="email" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Email"/>
-            </div>
-            <div className="mb-4">
-              <input type="text" id="address" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Address"/>
-            </div>
-            <button type="submit" className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Pay Now</button>
-          </form>
-        </div>
+        <div className="w-full h-[2px] bg-orange-700 mb-4"/>
+        <h1 className="text-3xl flex font-bold justify-center mb-3">Product Description</h1>
+        <Description />
+        <div className="w-full h-[2px] bg-orange-700 mt-4 mb-4"/>
+        <Footer />
       </div>
-    </div>
+      
+    </>
   );
 }
