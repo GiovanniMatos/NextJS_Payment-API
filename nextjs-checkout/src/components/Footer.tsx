efbec2ab3f0d6c1a5b42beb9c641dc71f115df3f
GiovanniMatos <giovannyafonso23@gmail.com>
2024-10-21 10:32:30 -0400

    payment methods | responsive

--- nextjs-checkout/src/components/Footer.tsx
+++ nextjs-checkout/src/components/Footer.tsx
@@ -2,17 +2,17 @@
 
 export default function Footer(){
     return(
-        <footer className="w-full p-6 text-lg flex bg-red-600">
-            <div className="flex justify-between">
-                <div className="bg-cyan-500">
+        <footer className="w-full p-6 text-lg flex bg-zinc-900">
+            <div className="flex justify-between lg:mx-auto">
+                <div className="text-zinc-500 lg:mr-4">
                     <h1>Company Image</h1>
                     <p>Company description</p>
                 </div>
-                <div className="bg-cyan-500">
+                <div className="text-zinc-500 lg:mr-4">
                     <h1>Company Image</h1>
                     <p>Company description</p>
                 </div>
-                <div className="bg-cyan-500">
+                <div className="text-zinc-500">
                     <h1>Company Image</h1>
                     <p>Company description</p>
                 </div>
